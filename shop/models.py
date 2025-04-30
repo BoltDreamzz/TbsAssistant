64a01be2b112eddf4e76d50bad9bb1f987a23ff5
BoltDreamzz <boltdreamz@gmail.com>
2025-04-30 16:52:43 -0700

    Implemented some detail page

--- shop/models.py
+++ shop/models.py
@@ -59,6 +59,9 @@ class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def get_total_price(self):
+        return sum(item.get_total_price() for item in self.items.all())
+
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
